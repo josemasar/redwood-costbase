8af77f03a3df012984e074c42e820ee1b7ba4235
Josema Sar <josemanuelsarrion@gmail.com>
2021-12-07 21:14:23 +0100

    upgraded to v1.0

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: ['src/**/*.{js,jsx,ts,tsx}'],
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
