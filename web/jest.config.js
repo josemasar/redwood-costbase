420445d696bc9254685a862495be6cb0b7fa73b1
Josema Sar <josemanuelsarrion@gmail.com>
2022-02-14 21:34:36 +0100

    fix jest config

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,8 +1,1 @@
-// More info at https://redwoodjs.com/docs/project-configuration-dev-test-build
-
-const config = {
-  rootDir: '../',
-  preset: '@redwoodjs/testing/config/jest/web',
-}
-
-module.exports = config
+module.exports = require('@redwoodjs/testing/config/jest/web')
